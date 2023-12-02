2284476b8bdd41c2ead19060bfd7911d57194ed4
GriffinJ-D <65211446+GriffinJ-D@users.noreply.github.com>
2023-12-01 20:27:11 -0800

    Create 325show-contents.sql
    Signed-off-by: GriffinJ-D <65211446+GriffinJ-D@users.noreply.github.com>

---
+++ 325show-contents.sql
@@ -0,0 +1,46 @@
+-- Group Name: Bytes and nibbles
+-- Group Members: Griffin Jarrell-Desch, Natalie Oulman, Simon Saltikov, Shawn Zereh, Aidan Sevillia, Brian Pinkernell
+-- Course: CS 325 - Fall 2023
+-- Last Modified: 
+
+SPOOL ON;
+SPOOL 325result-contents.txt
+
+-- For the user table
+PROMPT Displaying all users
+SELECT * FROM user_account;
+
+
+PROMPT Displaying partial rows from the user table
+SELECT user_id, email, password, fname, lname FROM user_account;
+
+
+-- For the customer table
+PROMPT customer table
+SELECT * FROM customer_account;
+
+
+PROMPT Admin
+SELECT * FROM admin_account;
+
+PROMPT vendor
+SELECT user_id, location FROM vendor_account;
+
+PROMPT subscription
+SELECT * FROM subscription;
+  
+PROMPT order
+SELECT * FROM order WHERE total_price > 50.00;
+
+PROMPT treat catalog
+SELECT item_id_num, item_name, quantity_on_hand FROM treat_catalog WHERE quantity_on_hand > 0;
+
+PROMPT treat type
+SELECT * FROM treat_type;
+
+PROMPT treat ingredients
+SELECT item_id_num, treat_ingredients FROM treat_ingredients WHERE item_id_num = 'item0001';
+
+
+
+SPOOL OFF;
